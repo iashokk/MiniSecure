44a3d7c8be0426f6bfd1280620d725e1856db95d
iashokk <vinoashok61@gmail.com>
2025-10-23 20:11:22 +0530

    chore(main): demo TransportSim send/recv placeholder

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include "minisec/handshake.h"
+#include "minisec/transport_sim.h"
 #include <iostream>
 int main() {
   minisec::Handshake hs("psk-demo");
   auto res = hs.run({"c123","1"}, {"s456", true});
   std::cout << (res.ok ? "OK " : "FAIL ") << res.session_key << "\n";
+  minisec::TransportSim net;
+  net.send({"hello"});
+  (void)net.recv();
   return res.ok ? 0 : 1;
 }

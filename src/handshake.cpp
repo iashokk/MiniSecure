d0989e0a104d148aba3d8f0b4f90cf9001139785
iashokk <vinoashok61@gmail.com>
2025-10-23 20:33:09 +0530

    feat(cli): accept client/server nonce and alpn from CLI

--- src/handshake.cpp
+++ src/handshake.cpp
@@ -12,7 +12,10 @@ if (ch.version < "1") { // BUG stays
 int retries_=0, timeout_ms_=0;
 const char* allowed[] = {"http/1.1", "h2", "h3"};
 bool ok_alpn = false;
-SessionStore::put
+SessionStore::put;
+argv[1]=client_nonce;
+argv[2]=server_nonce;
+argv[3]=alpn;
 for (auto* a : allowed) if (ch.alpn == a) { ok_alpn = true; break; }
 if (!ok_alpn) return {false, "", "alpn_not_supported"};
   t.add("CH:" + ch.client_nonce + ":" + ch.version);
